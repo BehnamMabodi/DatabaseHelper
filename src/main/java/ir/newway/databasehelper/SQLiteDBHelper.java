9bc8e872e858f28d8059d7d8d0fa55d03c4aa6e9
Behnam Mabodi <gpxchat@gmail.com>
2016-09-19 20:48:47 +0430

    @ Separated Database library base class from application database

--- src/main/java/ir/newway/databasehelper/SQLiteDBHelper.java
+++ src/main/java/ir/newway/databasehelper/SQLiteDBHelper.java
@@ -2,6 +2,7 @@ package ir.newway.databasehelper;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
@@ -14,9 +15,11 @@ import java.util.List;
  */
 public class SQLiteDBHelper extends SQLiteAssetHelper {
 
-    protected SQLiteDatabase mDatabase;
-    private static SQLiteDBHelper mInstance;
     protected Context mContext;
+    protected SQLiteDatabase mDatabase;
+    protected String mName;
+    protected int mVersion;
+    private static List<SQLiteDBHelper> mInstance;
     private List<MasterAsyncTask> mTaskLists;
 
     public interface onInsertTaskListener {
@@ -32,12 +35,47 @@ public class SQLiteDBHelper extends SQLiteAssetHelper {
         super(context, name, storageDirectory, factory, version);
         mContext = context.getApplicationContext();
         mTaskLists = new ArrayList<>();
+        mName = name;
+        mVersion = version;
     }
 
     public SQLiteDBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
         mContext = context.getApplicationContext();
         mTaskLists = new ArrayList<>();
+        mName = name;
+        mVersion = version;
+    }
+
+    // <Database Instance Creator> //
+    public static synchronized void getInstance(Context context, String dbName, int dbVersion, onGetInstanceListener listener) {
+        SQLiteDBHelper DBHelper = getInstanceIfExist(dbName);
+        if (DBHelper == null) {
+            //TODO: Add synchronized block here
+            DBHelper = new SQLiteDBHelper(context, dbName, null, dbVersion);
+            mInstance.add(DBHelper);
+            MasterAsyncTask task = DBHelper.createNewTask();
+            task.setupDatabaseInBackground(context, DBHelper, listener, new onGetInstanceListener() {
+                @Override
+                public void onTaskDone(MasterAsyncTask task, SQLiteDBHelper database) {
+                    database.RemoveTask(task);
+                }
+            });
+        } else {
+            listener.onTaskDone(null, DBHelper);
+        }
+    }
+
+    private static SQLiteDBHelper getInstanceIfExist(String dbName) {
+        if (mInstance != null) {
+            for (SQLiteDBHelper instance : mInstance) {
+                if (instance.getName().equals(dbName))
+                    return instance;
+            }
+        } else {
+            mInstance = new ArrayList<>();
+        }
+        return null;
     }
 
     protected synchronized SQLiteDatabase setupDatabase() {
@@ -45,28 +83,28 @@ public class SQLiteDBHelper extends SQLiteAssetHelper {
         return mDatabase;
     }
 
-    protected static synchronized void setupDatabaseInBackground(Context context, onGetInstanceListener listener) {
-        MasterAsyncTask task = mInstance.createNewTask();
-        task.setupDatabaseInBackground(context, mInstance, listener, new onGetInstanceListener() {
-            @Override
-            public void onTaskDone(MasterAsyncTask task, SQLiteDBHelper database) {
-                mInstance.onTaskDone(task);
-            }
-        });
+    public String getName() {
+        return mName;
     }
 
-    protected void setInstance(SQLiteDBHelper instance) {
-        mInstance = instance;
-    }
+    // </Database Instance Creator> //
 
 
-    protected void insert(String tableName, ContentValues values, onInsertTaskListener listener) {
+    public void insert(String tableName, ContentValues values, onInsertTaskListener listener) {
         createNewTask().insert(mDatabase, tableName, values, listener, new onInsertTaskListener() {
             @Override
             public void onTaskDone(MasterAsyncTask task) {
-                mInstance.onTaskDone(task);
+                RemoveTask(task);
             }
         });
+    }
+
+    public Cursor select(String tableName, String[] sqlSelect) {
+        // Cursor cursor = mDatabase.query(ZekrDatabase.TABLE_ZEKR, sqlSelect, "user_id = ?", new String[]{userId}, null, null, null);
+        Cursor cursor = mDatabase.query(tableName, sqlSelect, null, null, null, null, null);
+        // Cursor cursor = mDatabase.rawQuery("SELECT * FROM zekr", null);
+        cursor.moveToFirst();
+        return cursor;
     }
 
     protected MasterAsyncTask createNewTask() {
@@ -75,8 +113,19 @@ public class SQLiteDBHelper extends SQLiteAssetHelper {
         return newTask;
     }
 
-    protected void onTaskDone(MasterAsyncTask task) {
+    public SQLiteDatabase getWriteableDatabase() {
+        return mDatabase;
+    }
+
+    protected void RemoveTask(MasterAsyncTask task) {
         mTaskLists.remove(task);
+    }
+
+    public static void closeAllTasks() {
+        if (mInstance != null)
+            for (SQLiteDBHelper instance : mInstance) {
+                instance.closeTasks();
+            }
     }
 
     public void closeTasks() {
